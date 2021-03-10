fb2c3f1e8b2616bc710106cd288cbcbeaa46cf39
AlexeyVFrolov <isbase@mail.ru>
2021-03-10 23:02:36 +0300

    Code reformated, local variable names changed

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        long Balance = 0; // balance, kopecks
-        System.out.println(String.format("Balance: %s", Balance));
+        long balance = 0; // balance, kopecks
+        System.out.println(String.format("Balance: %s", balance));
         System.out.print("Please, enter the account refill amount in rubles ");
-        long AccountRefillAmount = (long)(in.nextDouble() * 100); // account refill amount, kopecks
-        long BonusAmount = AccountRefillAmount > 100000 ? AccountRefillAmount / 100 : 0; // 1 ruble per 100 rubles
-        Balance += AccountRefillAmount + BonusAmount; // new balance, kopecks
-        System.out.println(String.format("Your bonus amount accrued, rubles: %s", BonusAmount / 100));
-        System.out.println(String.format("New balance, rubles: %s", (double) Balance / 100));
+        long accountRefillAmount = (long) (in.nextDouble() * 100); // account refill amount, kopecks
+        long bonusAmount = accountRefillAmount > 100000 ? accountRefillAmount / 10000 * 100 : 0; // 1 ruble per 100 rubles
+        balance += accountRefillAmount + bonusAmount; // new balance, kopecks
+        System.out.println(String.format("Your bonus amount accrued, rubles: %s", bonusAmount / 100));
+        System.out.println(String.format("New balance, rubles: %s", (double) balance / 100));
     }
 }
